4a329243a13058a116da0cb7dc0942ababa68513
himanshuCoder <himanshu.bijwar.bca@ghriet.raisoni.net>
2024-08-12 23:52:11 +0530

    This is day 1 tihis program for the largest values of ages are doing the calculate

---
+++ Day1/AgeValues.py
@@ -0,0 +1,10 @@
+a=int(input('Enter the first age:-'))
+b=int(input('Enter the Second age:-'))
+c=int(input('Enter the Third age:-'))
+
+max=a
+if max<b:
+    max=b
+if max <c:
+    max=c
+print(max)
